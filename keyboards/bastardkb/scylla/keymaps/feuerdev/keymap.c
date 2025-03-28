f8dfc962f0145309c25f36845536cd5b5bc10afc
jannik-feuerhahn <jannik.feuerhahn@ada.com>
2025-03-28 12:15:25 +0100

    fix: cleanup plus first Miryokus style layer

--- keyboards/bastardkb/scylla/keymaps/feuerdev/keymap.c
+++ keyboards/bastardkb/scylla/keymaps/feuerdev/keymap.c
@@ -8,8 +8,7 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
@@ -19,36 +18,18 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    [0] = LAYOUT_split_4x6_5(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC,
+    // 0 - Alpha layer
+    [0] = LAYOUT_split_4x6_5(KC_NO, KC_1, KC_2, KC_3, KC_4, KC_5,                  KC_6, KC_7, KC_8, KC_9, KC_0, KC_NO,
                              //-------------------------------------------------//-----------------------------------------------------------//
-                             KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS,
+                             KC_NO, KC_Q, KC_W, KC_F, KC_P, KC_B,                  KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_NO,
                              //-------------------------------------------------//-----------------------------------------------------------//
-                             KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+                             KC_NO, KC_A, KC_R, KC_S, KC_T, KC_G,                  KC_M, KC_N, KC_E, KC_I, KC_O, KC_NO,
                              //-------------------------------------------------//-----------------------------------------------------------//
-                             KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_BSLS,
+                             KC_NO, KC_Z, KC_X, KC_C, KC_D, KC_V,                  KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH, KC_NO,
                              //-------------------------------------------------//-----------------------------------------------------------//
-                             KC_LCTL, KC_SPC, MO(1),                                MO(2), KC_ENT, KC_RGUI, 
-                             KC_HOME, KC_A,                                      KC_B, KC_RALT),
-
-    [1] = LAYOUT_split_4x6_5(KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
-                             //---------------------------------------------------------//-----------------------------------------------------------//
-                             QK_BOOT, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_P7, KC_P8, KC_P9, _______, KC_PLUS,
-                             //---------------------------------------------------------//-----------------------------------------------------------//
-                             _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END, KC_LPRN, KC_RPRN, KC_P4, KC_P5, KC_P6, KC_MINS, KC_PIPE,
-                             //---------------------------------------------------------//-----------------------------------------------------------//
-                             _______, _______, _______, _______, _______, _______, _______, KC_P1, KC_P2, KC_P3, KC_EQL, KC_UNDS,
-                             //---------------------------------------------------------//-----------------------------------------------------------//
-                             KC_LCTL, KC_HOME, KC_TRNS,                                KC_TRNS, KC_RALT, KC_RGUI, 
-                             KC_SPC, KC_BSPC,                                         KC_RCTL, KC_ENT),
-
-    [2] = LAYOUT_split_4x6_5(KC_F12, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
-                             //---------------------------------------------------------//--------------------------------------------------------------//
-                             _______, _______, RGB_RMOD, RGB_TOG, RGB_MOD, KC_LBRC, KC_RBRC, _______, KC_NUM, KC_INS, KC_SCRL, KC_MUTE,
-                             //---------------------------------------------------------//--------------------------------------------------------------//
-                             _______, KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, KC_LPRN, KC_RPRN, KC_MPRV, KC_MPLY, KC_MNXT, _______, KC_VOLU,
-                             //---------------------------------------------------------//--------------------------------------------------------------//
-                             _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD,
-                             //---------------------------------------------------------//--------------------------------------------------------------//
-                             KC_LCTL, KC_HOME, KC_TRNS,                                KC_TRNS, KC_RALT, QK_BOOT, 
-                             KC_SPC, KC_BSPC,                                         KC_RCTL, KC_ENT),
+                             //-------------------------------------------------//-----------------------------------------------------------//
+                             LT(1, KC_ESC), LT(2, KC_SPC), LT(3, KC_TAB),           LT(4, KC_ENT), LT(5, KC_BSPC), LT(6, KC_DEL), 
+                             //-------------------------------------------------//-----------------------------------------------------------//
+                             KC_NO, KC_NO,                                        KC_NO, KC_NO),
+    
 };
