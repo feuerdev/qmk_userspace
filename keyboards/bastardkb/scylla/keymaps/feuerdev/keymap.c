af7ace0be7ff3c62752592824af7ff11311b7753
feuerdev <jannik@feuer.dev>
2025-04-11 13:03:06 +0200

    fix: put tab on the corner

--- keyboards/bastardkb/scylla/keymaps/feuerdev/keymap.c
+++ keyboards/bastardkb/scylla/keymaps/feuerdev/keymap.c
@@ -44,20 +44,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [ALPHA] = LAYOUT_split_4x6_5(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,              KC_6, KC_7, KC_8, KC_9, KC_0, BOOT_R,
                              //-------------------------------------------------//-----------------------------------------------------------//
-                             EE_RST_L, KC_Q, KC_W, KC_F, KC_P, KC_B,                KC_J, KC_L, KC_U, KC_Y, KC_MINS, EE_RST_R,
+                             KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_B,                  KC_J, KC_L, KC_U, KC_Y, KC_MINS, EE_RST_R,
                              //-------------------------------------------------//-----------------------------------------------------------//
                              KC_NO, HOME_A, KC_R, KC_S, KC_T, KC_G,                 KC_M, KC_N, KC_E, KC_I, HOME_O, KC_NO,
                              //-------------------------------------------------//-----------------------------------------------------------//
                              KC_NO, KC_Z, KC_X, KC_C, KC_D, KC_V,                   KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH, KC_NO,
                              //-------------------------------------------------//-----------------------------------------------------------//
                              //-------------------------------------------------//-----------------------------------------------------------//
-                             LT(FUNCTION, KC_NO), LT(NAV, KC_SPC), LGUI_T(KC_TAB),  LGUI_T(KC_ENT), LT(SYMBOL, KC_BSPC), LT(FUNCTION, KC_DEL), 
+                             LT(FUNCTION, KC_NO), LT(NAV, KC_SPC), LGUI_T(KC_NO),   LGUI_T(KC_ENT), LT(SYMBOL, KC_BSPC), LT(FUNCTION, KC_NO), 
                              //-------------------------------------------------//-----------------------------------------------------------//
                              KC_LCTL, KC_LALT,                                      KC_LALT, KC_HYPR),
 
     [NAV] = LAYOUT_split_4x6_5(KC_TRNS, HYPR(KC_TRNS), HYPR(KC_TRNS), HYPR(KC_TRNS), HYPR(KC_TRNS), HYPR(KC_TRNS),    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
                             //------------------------------------------------------------------------------------//-----------------------------------------------------------//
-                            KC_NO, HYPR(KC_TRNS), HYPR(KC_TRNS), HYPR(KC_TRNS), HYPR(KC_TRNS), HYPR(KC_TRNS),       KC_NO, LGUI(KC_GRV), KC_UP, LGUI(KC_LCBR), LGUI(KC_RCBR), KC_NO,
+                            KC_TRNS, HYPR(KC_TRNS), HYPR(KC_TRNS), HYPR(KC_TRNS), HYPR(KC_TRNS), HYPR(KC_TRNS),       KC_NO, LGUI(KC_GRV), KC_UP, LGUI(KC_LCBR), LGUI(KC_RCBR), KC_NO,
                             //------------------------------------------------------------------------------------//-----------------------------------------------------------//
                             KC_NO, HYPR(KC_TRNS), HYPR(KC_TRNS), HYPR(KC_TRNS), HYPR(KC_TRNS), HYPR(KC_TRNS),       KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_RSFT, KC_NO,
                             //------------------------------------------------------------------------------------//-----------------------------------------------------------//
@@ -70,27 +70,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [SYMBOL] = LAYOUT_split_4x6_5(KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5,                 KC_6, KC_7, KC_8, KC_9, KC_0, KC_NO,
                             //-------------------------------------------------------//--------------------------------------------------------------//
-                            KC_NO, KC_BSLS, KC_LABK, KC_RABK, KC_ASTR, KC_NO,          KC_AMPR, KC_GRV, KC_LBRC, KC_RBRC, KC_PERC, KC_NO,
+                            KC_TRNS, KC_BSLS, KC_LABK, KC_RABK, KC_ASTR, KC_NO,          KC_AMPR, KC_GRV, KC_LBRC, KC_RBRC, KC_PERC, KC_NO,
                             //-------------------------------------------------------//--------------------------------------------------------------//
                             KC_NO, KC_TILD, KC_MINS, KC_PLUS, KC_EQL, KC_HASH,         KC_PIPE, KC_COLN, KC_LPRN, KC_RPRN, KC_SCLN, KC_NO,
                             //-------------------------------------------------------//--------------------------------------------------------------//
                             KC_NO, KC_CIRC, KC_SLSH, KC_DQUO, KC_QUOT, LALT_T(KC_U),   EMAIL, KC_DLR, KC_LCBR, KC_RCBR, KC_AT, KC_NO,
                             //-------------------------------------------------------//--------------------------------------------------------------//
                             //-------------------------------------------------------//-----------------------------------------------------------//
-                            KC_NO, KC_SPC, LGUI_T(KC_TAB),                             KC_NO, KC_NO, KC_NO, 
+                            KC_NO, KC_SPC, LGUI_T(KC_NO),                              KC_NO, KC_NO, KC_NO, 
                             //-------------------------------------------------------//-----------------------------------------------------------//
                             KC_LCTL, KC_LALT,                                          KC_LALT, KC_HYPR),
 
     [FUNCTION] = LAYOUT_split_4x6_5(KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
                             //----------------------------------------------------//-----------------------------------------------------------//
-                            KC_NO, KC_F12, KC_F7, KC_F8, KC_F9, KC_NO,              KC_NO, KC_NO, KC_VOLU, KC_NO, KC_NO, KC_NO,
+                            KC_TRNS, KC_F12, KC_F7, KC_F8, KC_F9, KC_NO,              KC_NO, KC_NO, KC_VOLU, KC_NO, KC_NO, KC_NO,
                             //----------------------------------------------------//-----------------------------------------------------------//
                             KC_NO, KC_F11, KC_F4, KC_F5, KC_F6, KC_NO,              KC_NO, KC_MPRV, KC_VOLD, KC_MNXT, KC_NO, KC_NO,
                             //----------------------------------------------------//-----------------------------------------------------------//
                             KC_NO, KC_F10, KC_F1, KC_F2, KC_F3, KC_NO,              KC_NO, KC_MPLY, KC_MSTP, KC_MUTE, KC_NO, KC_NO,
                             //-------------------------------------------------//-----------------------------------------------------------//
                             //-------------------------------------------------//-----------------------------------------------------------//
-                            KC_ESC, KC_SPC, LGUI_T(KC_TAB),                         LGUI_T(KC_ENT), KC_BSPC, KC_DEL, 
+                            KC_ESC, KC_SPC, LGUI_T(KC_NO),                          LGUI_T(KC_ENT), KC_BSPC, KC_NO, 
                             //-------------------------------------------------//-----------------------------------------------------------//
                             KC_LCTL, KC_LALT,                                       KC_LALT, KC_HYPR),
 };
