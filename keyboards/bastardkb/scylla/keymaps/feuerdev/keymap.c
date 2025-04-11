644b7c3583df0d4ddb6097e799c3bc441e9a1d33
feuerdev <jannik@feuer.dev>
2025-04-11 15:28:05 +0200

    fix: hyper keys

--- keyboards/bastardkb/scylla/keymaps/feuerdev/keymap.c
+++ keyboards/bastardkb/scylla/keymaps/feuerdev/keymap.c
@@ -53,13 +53,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                              //-------------------------------------------------------//-----------------------------------------------------------//
                              KC_NO, KC_NO,                                              KC_NO, KC_NO),
 
-    [NAV] = LAYOUT_split_4x6_5(KC_TRNS, HYPER(KC_1), HYPER(KC_2), HYPER(KC_3), HYPER(KC_4), HYPER(KC_5),          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    [NAV] = LAYOUT_split_4x6_5(KC_TRNS, HYPR(KC_1), HYPR(KC_2), HYPR(KC_3), HYPR(KC_4), HYPR(KC_5),          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
                             //-------------------------------------------------------------------------------//-----------------------------------------------------------//
-                            KC_TRNS, HYPER(KC_Q), HYPER(KC_W), HYPER(KC_F), HYPER(KC_P), HYPER(KC_B),             KC_NO, LGUI(KC_GRV), KC_UP, LGUI(KC_LCBR), LGUI(KC_RCBR), KC_NO,
+                            KC_TRNS, HYPR(KC_Q), HYPR(KC_W), HYPR(KC_F), HYPR(KC_P), HYPR(KC_B),             KC_NO, LGUI(KC_GRV), KC_UP, LGUI(KC_LCBR), LGUI(KC_RCBR), KC_NO,
                             //-------------------------------------------------------------------------------//-----------------------------------------------------------//
-                            KC_NO, KC_LSFT, HYPER(KC_R), HYPER(KC_S), HYPER(KC_T), HYPER(KC_G),                   KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_RSFT, KC_NO,
+                            KC_NO, KC_LSFT, HYPR(KC_R), HYPR(KC_S), HYPR(KC_T), HYPR(KC_G),                   KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_RSFT, KC_NO,
                             //-------------------------------------------------------------------------------//-----------------------------------------------------------//
-                            KC_NO, HYPER(KC_Z), HYPER(KC_X), HYPER(KC_C), HYPER(KC_D), HYPER(KC_V),               KC_NO, SW_WIN, KC_NO, KC_NO, KC_NO, KC_NO,
+                            KC_NO, HYPR(KC_Z), HYPR(KC_X), HYPR(KC_C), HYPR(KC_D), HYPR(KC_V),               KC_NO, SW_WIN, KC_NO, KC_NO, KC_NO, KC_NO,
                             //-------------------------------------------------------------------------------//-----------------------------------------------------------//
                             //-------------------------------------------------------------------------------//-----------------------------------------------------------//
                             LALT_T(KC_ESC), LGUI_T(KC_SPC), LT(SYMBOL, KC_TAB),                                   LCTL_T(KC_ENT), KC_TRNS, LT(FUNCTION, KC_NO),   
