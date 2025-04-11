5a8245428b4f1ce83477a33e228422dbae340574
feuerdev <jannik@feuer.dev>
2025-04-11 12:43:57 +0200

    fix: update media controls

--- keyboards/bastardkb/scylla/keymaps/feuerdev/keymap.c
+++ keyboards/bastardkb/scylla/keymaps/feuerdev/keymap.c
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                             //------------------------------------------------------------------------------------//-----------------------------------------------------------//
                             KC_LCTL, KC_LALT,                                                                       KC_LALT, KC_HYPR),
 
-    [SYMBOL] = LAYOUT_split_4x6_5(KC_NO, KC_1, KC_2, KC_3, KC_4, KC_5,                  KC_6, KC_7, KC_8, KC_9, KC_0, KC_NO,
+    [SYMBOL] = LAYOUT_split_4x6_5(KC_NO, KC_1, KC_2, KC_3, KC_4, KC_5,             KC_6, KC_7, KC_8, KC_9, KC_0, KC_NO,
                             //-------------------------------------------------//--------------------------------------------------------------//
                             KC_NO, KC_BSLS, KC_LABK, KC_RABK, KC_ASTR, KC_NO,      KC_AMPR, KC_GRV, KC_LBRC, KC_RBRC, KC_PERC, KC_NO,
                             //-------------------------------------------------//--------------------------------------------------------------//
@@ -83,14 +83,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [FUNCTION] = LAYOUT_split_4x6_5(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
                             //----------------------------------------------------//-----------------------------------------------------------//
-                            KC_NO, KC_F12, KC_F7, KC_F8, KC_F9, KC_NO,              KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                            KC_NO, KC_F12, KC_F7, KC_F8, KC_F9, KC_NO,              KC_NO, KC_NO, KC_VOLU, KC_NO, KC_NO, KC_NO,
                             //----------------------------------------------------//-----------------------------------------------------------//
-                            KC_NO, KC_F11, KC_F4, KC_F5, KC_F6, KC_NO,              KC_NO, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_NO,
+                            KC_NO, KC_F11, KC_F4, KC_F5, KC_F6, KC_NO,              KC_NO, KC_MPRV, KC_VOLD, KC_MNXT, KC_NO, KC_NO,
                             //----------------------------------------------------//-----------------------------------------------------------//
-                            KC_NO, KC_F10, KC_F1, KC_F2, KC_F3, KC_NO,              KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                            KC_NO, KC_F10, KC_F1, KC_F2, KC_F3, KC_NO,              KC_NO, KC_MPLY, KC_MSTP, KC_MUTE, KC_NO, KC_NO,
                             //-------------------------------------------------//-----------------------------------------------------------//
                             //-------------------------------------------------//-----------------------------------------------------------//
-                            KC_ESC, KC_SPC, LGUI_T(KC_TAB),                         KC_MSTP, KC_MPLY, KC_MUTE, 
+                            KC_ESC, KC_SPC, LGUI_T(KC_TAB),                         LGUI_T(KC_ENT), KC_BSPC, KC_DEL, 
                             //-------------------------------------------------//-----------------------------------------------------------//
                             KC_LCTL, KC_LALT,                                       KC_LALT, KC_HYPR),
 };
